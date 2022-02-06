aa0eac852bc39881fe01d85a3fef929797bd83d6
kevftka <dev@kevftka.xyz>
2022-02-06 04:22:27 -0500

    modify the folder

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'AutoHaus Parts',
+  title: 'AutoHaus Parts',
+  description: 'Todo en respuestos ',
   locale: 'en',
 };
